17cf52d6c03b0172292027e65afa94c7f66c719e
Phuc Tran <glup3.tran@gmail.com>
2021-02-08 17:37:55 +0100

    add pagination

--- handler/handler.go
+++ handler/handler.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"strconv"
+
 	"github.com/glup3/jetti-ui/model"
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
@@ -10,6 +12,13 @@ import (
 func GetIndex(db *gorm.DB) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		var matches []model.Match
+
+		page, _ := strconv.Atoi(c.Query("page"))
+		pageSize := 5
+
+		if page == 0 {
+			page = 1
+		}
 
 		db.
 			Preload("Team1.Player1").
@@ -23,10 +32,15 @@ func GetIndex(db *gorm.DB) fiber.Handler {
 			Preload("Team2.Player4").
 			Preload("Team2.Player5").
 			Order("ID desc").
+			Offset((page - 1) * pageSize).
+			Limit(pageSize).
 			Find(&matches)
 
 		return c.Render("index", fiber.Map{
-			"matches": matches,
+			"matches":  matches,
+			"page":     page,
+			"nextPage": page + 1,
+			"prevPage": page - 1,
 		}, "layout/main")
 	}
 }
